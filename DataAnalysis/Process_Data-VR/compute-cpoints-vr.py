2e484b65cb59314fe4eca3d3bd2c8db08276b8d5
ymujammil <muzammilnxs@gmail.com>
2017-12-01 23:35:50 -0700

    Allowed user to set parameters.seperated static and dynamic plots

--- DataAnalysis/Process_Data-VR/compute-cpoints-vr.py
+++ DataAnalysis/Process_Data-VR/compute-cpoints-vr.py
@@ -66,11 +66,21 @@ try:
     distance_data = np.genfromtxt(global_path  + "\distance.npy", delimiter=',',
                           skip_header=1,names = ['Distance'], dtype=(float))
     start_time = gaze_data['timestamp'][0]
+    subject_id = input("Set the subject ID: ")
+    exp_type = input("Set the experiment type: ")
+    int_pup_distance = 0
+    while True:
+        try:
+            int_pup_distance= int(input("Set the measured Interpupillary distance (mm): "))
+        except ValueError:
+            print("\t\t\tNot a valid input. Please enter an integer value")
+            continue
+        else:
+            break
     for p in gaze_data:
         if(p['confidence'] >= 0.70):
             #Split 'base_data' string using '-' character to get the eye_index
             eye_index = int(p['base_data'].decode().split("-")[1])
-            #print((p['timestamp']*1000).astype(int))
             if(eye_index == 0):
                 left_eye_convpt[(p['timestamp']*1000).astype(int)] = p
             if (eye_index == 1):
@@ -111,22 +121,153 @@ try:
             time_in_seconds[key] = (key / 1000 )- start_time
 
             # Convergence point calculation (E * D) / (E + M)
-            conv_point = 65 * 18/(65 + (3.15 *diff_xpos))
+            conv_point = int_pup_distance * 18/(int_pup_distance + (99.5 *diff_xpos))
 
             distance_ball[key] = distance_data['Distance'][right_eye_convpt[key]['index']]
             diff_list[key] = conv_point
     od = collections.OrderedDict(sorted(diff_list.items()))
 
-    path_to_convpt_file = os.path.join(global_path + r"\exports", os.listdir(global_path + r"\exports").pop()) + "\convergence_points.csv"
+    path_to_convpt_file = os.path.join(global_path + r"\exports", os.listdir(global_path + r"\exports").pop()) + r"\output" + "\conv_points_" + exp_type + "_" +subject_id + "_VR.csv"
+    path_to_plot_file = os.path.join(global_path + r"\exports", os.listdir(global_path + r"\exports").pop()) + r"\output" + "\plot_" + exp_type + "_" +subject_id + "_VR.png"
+    if not os.path.exists(os.path.dirname(path_to_convpt_file)):
+        try:
+            os.makedirs(os.path.dirname(path_to_convpt_file))
+        except OSError as exc:  # Guard against race condition
+                raise
     with open(path_to_convpt_file, 'w',newline="\n", encoding="utf-8") as csv_file:
         writer = csv.writer(csv_file)
+        writer.writerow(["time","convergence_point","ball_distance"])
         for key, value in od.items():
            writer.writerow([time_in_seconds[key], value, distance_ball[key]])
-    print("Successfully computed Convergence points, 'convergence_points.csv' file written to " + path_to_convpt_file)
+    print("\t\t\tConvergence points saved to " + path_to_convpt_file)
 except PermissionError:
     print("Error:")
-    print("Oops! Cound not write to convergence_points.csv...")
+    print("Oops! Cound not write output to file...")
     print("Is the file open in the system? Please close the file and try again")
     exit()
 
+# Create a plot with the output of "convergence_points.csv"
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(path_to_convpt_file)
+
+# Create a figure
+plt.figure(figsize=(10,8))
+
+if(exp_type == "static"):
+    # Create a scatterplot of,
+                # time with ball distance 0.5m as the x axis
+    plt.scatter(df['time'][df['ball_distance'] == 0.5],
+                # convergence_point with ball distance 0.5m as the y axis
+                df['convergence_point'][df['ball_distance'] == 0.5],
+                # the marker as
+                marker='x',
+                # the color
+                color='b',
+                # the alpha
+                alpha=0.7,
+                # with size
+                s = 124,
+                # labelled this
+                label='Distance 0.5m')
+    # Repeat For Distance 1m
+    plt.scatter(df['time'][df['ball_distance'] == 1],
+                df['convergence_point'][df['ball_distance'] == 1],
+                marker='o',
+                color='r',
+                alpha=0.7,
+                s = 124,
+                label='Distance 1')
+    # Repeat For Distance 1.5m
+    plt.scatter(df['time'][df['ball_distance'] == 1.5],
+                df['convergence_point'][df['ball_distance'] == 1.5],
+                marker='+',
+                color='y',
+                alpha=0.7,
+                s = 124,
+                label='Distance 1.5')
+    # Repeat For Distance 2m
+    plt.scatter(df['time'][df['ball_distance'] == 2],
+                df['convergence_point'][df['ball_distance'] == 2],
+                marker='*',
+                color='c',
+                alpha=0.7,
+                s = 124,
+                label='Distance 2')
+    # Repeat For Distance 3m
+    plt.scatter(df['time'][df['ball_distance'] == 3],
+                df['convergence_point'][df['ball_distance'] == 3],
+                marker='s',
+                color='m',
+                alpha=0.7,
+                s = 124,
+                label='Distance 3')
+    # Repeat For Distance 4m
+    plt.scatter(df['time'][df['ball_distance'] == 4],
+                df['convergence_point'][df['ball_distance'] == 4],
+                marker='v',
+                color='k',
+                alpha=0.7,
+                s = 124,
+                label='Distance 4')
+    # Repeat For Distance 6m
+    plt.scatter(df['time'][df['ball_distance'] == 6],
+                df['convergence_point'][df['ball_distance'] == 6],
+                marker='>',
+                color='r',
+                alpha=0.7,
+                s = 124,
+                label='Distance 6')
+    # Repeat For Distance 8m
+    plt.scatter(df['time'][df['ball_distance'] == 8],
+                df['convergence_point'][df['ball_distance'] == 8],
+                marker='^',
+                color='g',
+                alpha=0.7,
+                s = 124,
+                label='Distance 8')
+    # Chart title
+    plt.title('Convergence Point Vs Distance Vs Time (Static-VR)')
+    # x label
+    plt.xlabel('Time(sec)')
+    # set the figure boundaries x
+    plt.xlim([min(df['time']), max(df['time'])])
+    # set the figure boundaries y
+    plt.ylim([min(df['convergence_point']), max(df['convergence_point'])])
+else:
+    # Create a scatterplot of,
+                # ball_distance as the x axis
+    plt.scatter(df['ball_distance'],
+                # convergence_point as the y axis
+                df['convergence_point'],
+                # the marker as
+                marker='o',
+                # the color
+                color='b',
+                # the alpha
+                alpha=0.7,
+                # with size
+                s = 124)
+    # Chart title
+    plt.title('Convergence Point Vs Distance (Dynamic-VR)')
+    # x label
+    plt.xlabel('Distance(m)')
+    # set the figure boundaries x
+    plt.xlim([min(df['ball_distance']), max(df['ball_distance'])])
+    # set the figure boundaries y
+    plt.ylim([15,21])
+
+
+
+# y label
+plt.ylabel('Convergence Point(mm)')
+
+# and a legend
+plt.legend(loc='upper right')
+
+# Save the output plot
+plt.savefig(path_to_plot_file)
+print("\t\t\tScatter plot saved to " + path_to_plot_file)
+print("Successfully processed!\n")
 
